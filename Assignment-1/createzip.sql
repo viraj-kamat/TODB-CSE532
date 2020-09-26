2fab0343005cc7ea437206ac9cba481f08c6f16e
Viraj Kamat <viraj.kmt@gmail.com>
2020-09-25 22:55:35 -0400

    Added initial files

---
+++ Assignment-1/createzip.sql
@@ -0,0 +1,5 @@
+CREATE TABLE cse532.zip(
+zip INT,
+county VARCHAR(50),
+irs_estimated_population_2015 BIGINT
+)
---
+++ Assignment-1/zipload.sql
@@ -0,0 +1,2 @@
+db2 load from "C:\\TODB\\assignment-1\\zip_code_database.csv" of del method P(1, 8, 15) replace into cse532.zip (zip, county, irs_estimated_population_2015) 
+
